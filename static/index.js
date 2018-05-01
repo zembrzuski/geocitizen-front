26e45e974fc9ac4ef2098feb67b72d968a9cdf73
Your Name <you@example.com>
2018-05-01 14:53:40 -0300

    refactoring

--- static/index.js
+++ static/index.js
@@ -1,65 +1,37 @@
-function plota(track_id) {
-  var uluru = {lat: -25.363, lng: 131.044};
+function createPath(coordinates, strokeColor, strokeOpacity, strokeWeight) {
+  return new google.maps.Polyline({
+    path: coordinates,
+    strokeColor: strokeColor,
+    strokeOpacity: strokeOpacity,
+    strokeWeight: strokeWeight
+  });
+}
+
+function plotaTrack(track_id) {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: uluru
+    center: {lat: -25.363, lng: 131.044}
   });
 
-  var cao = $.get('http://127.0.0.1:5000/proxy_pass?path=track/by-id/' + track_id, function(data) {
-
+  $.get('http://127.0.0.1:5000/proxy_pass?path=track/by-id/' + track_id, function(data) {
     coordinates = data['points'];
-
-    var myPath = new google.maps.Polyline({
-      path: coordinates,
-      strokeColor: 'red',
-      strokeOpacity: 0.8,
-      strokeWeight: 5
-    });
-
+    var myPath = createPath(coordinates, 'red', 0.8, 5);
     myPath.setMap(map);
   });
-
 }
 
 function plotaUser(user_id) {
-  var uluru = {lat: -25.363, lng: 131.044};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
-    center: uluru
+    center: {lat: -25.363, lng: 131.044}
   });
 
-  var cao = $.get('http://127.0.0.1:5000/proxy_pass?path=user/by-user-id/' + user_id, function(data) {
-  //var cao = $.get('http://127.0.0.1:5000/proxy_pass?path=user/by-user-id-and-hour-of-day/011/22/24', function(data) {
-
+  $.get('http://127.0.0.1:5000/proxy_pass?path=user/by-user-id/' + user_id, function(data) {
     paths = data['paths'];
 
-    var menuEsquerdaItems = []
-
     for(x = 0 ; x < paths.length; x++) {
-
-      var menuEsquerdaItem = Object()
-      menuEsquerdaItem.id = paths[x]['id']
-      menuEsquerdaItem.inicio = paths[x]['firstTimestamp']
-      menuEsquerdaItem.fim = paths[x]['lastTimestamp']
-      menuEsquerdaItems = menuEsquerdaItems.concat(menuEsquerdaItem)
-
-      var myPath = new google.maps.Polyline({
-        path: paths[x]['points'],
-        strokeColor: 'black',
-        strokeOpacity: 0.1,
-        strokeWeight: 5
-      });
-
-      myPath.setMap(map);
+      createPath(paths[x]['points'], 'black', 0.1, 5).setMap(map);
     }
-
-    var stringao = ""
-    for(x = 0 ; x < menuEsquerdaItems.length; x++) {
-      xx = menuEsquerdaItems[x]
-      stringao += xx['inicio'] + "<br/>"
-    }
-    $(".right").html(stringao);
-
   });
 
 }
@@ -67,7 +39,7 @@ function plotaUser(user_id) {
 function initMap() {
     if (location.pathname.startsWith("/map/")) {
       var track_id = location.pathname.split("/map/")[1];
-      plota(track_id);
+      plotaTrack(track_id);
     }
 
     location.pathname.startsWith("/user/")
