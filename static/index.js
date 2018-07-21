0f8d71ac6b7e8cd59a84d212f7b44c00b10ccbc2
rodrigo zembrzuski <zembrzuski@gmail.com>
2018-07-21 12:22:57 -0300

    little refactor

--- static/index.js
+++ static/index.js
@@ -20,20 +20,24 @@ function plotaTrack(track_id) {
   });
 }
 
+function plotaUserCallback(data, map) {
+  paths = data['paths'];
+
+  for(x = 0 ; x < paths.length; x++) {
+    createPath(paths[x]['points'], 'black', 0.1, 5).setMap(map);
+  }
+}
+
 function plotaUser(user_id) {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: {lat: -25.363, lng: 131.044}
   });
 
-  $.get('http://127.0.0.1:5000/proxy_pass?path=user/by-user-id/' + user_id, function(data) {
-    paths = data['paths'];
-
-    for(x = 0 ; x < paths.length; x++) {
-      createPath(paths[x]['points'], 'black', 0.1, 5).setMap(map);
-    }
-  });
-
+  $.get(
+    'http://127.0.0.1:5000/proxy_pass?path=user/by-user-id/' + user_id, 
+    function(data) {plotaUserCallback(data, map);}
+  );
 }
 
 function initMap() {
